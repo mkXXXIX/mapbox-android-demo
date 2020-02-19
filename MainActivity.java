90a3ddceac919cdd761ea1f1e0df95a2f5fc5ddd
mkXXXIX <37010380+mkXXXIX@users.noreply.github.com>
2020-02-19 00:51:37 -0500

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,1 +1,10 @@
 
+import android.os.Bundle;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
